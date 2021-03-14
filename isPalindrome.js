c1e14fec9920e8e439faa5639faeae2092350a3c
Gnanamithra, Benison <Benison.Gnanamithra@cerner.com>
2021-03-14 14:36:40 +0530

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,16 +1,8 @@
 function isPalindrome(str){
-    function reverse(strInput) {
-        if(strInput.length === 0) {
-            return strInput;
-        }
-        return strInput[strInput.length - 1] + reverse(strInput.slice(0, -1));
-    }
-    let stringReversed = reverse(str);
-    if(str === stringReversed) {
-       return true 
-    }
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
     return false;
-  }
-  
+}
   isPalindrome("abcba");
   
